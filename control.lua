99f453ebe99772c96c26979e3723691512fe93fb
Optera <yousei.kyoko+github@gmail.com>
2017-09-07 13:07:11 +0200

    1.3.0
    - rewrote tick to use stride with offset from eradicator
    - use clear_items_inside() instead of get_inventory(defines.inventory.chest).clear()

--- control.lua
+++ control.lua
@@ -46,35 +46,44 @@ function OnEntityRemoved(event)
   end
 end
 
--- tested with 504 chests at 100 tick update interval ~ 0.040ms
+-- stepping from tick modulo with stride by eradicator
+-- tested with 480 chests over 100.000 ticks: 11776.126 ms
 function OnTick(event)
-  global.tickCount = global.tickCount or 1
-  global.chestIndex = global.chestIndex or 1
-
-  -- only work if index is within bounds
-  if global.chestIndex <= #global.VoidChests then
-    local stopIndex = global.chestIndex + global.VoidChestStride - 1
-    if stopIndex >= #global.VoidChests then
-      stopIndex = #global.VoidChests
-    end
-
-    -- log("[VCP] "..global.tickCount.." / "..game.tick.." clearing chest ids "..global.chestIndex.." to "..stopIndex)
-    for i=global.chestIndex, stopIndex do
-      global.VoidChests[i].get_inventory(defines.inventory.chest).clear()
-    end
-    global.chestIndex = stopIndex + 1
-  end
-
-  -- reset clock and chest index
-  if global.tickCount < update_interval then
-    global.tickCount = global.tickCount + 1
-  else
-    global.tickCount = 1
-    global.chestIndex = 1
+  local offset = game.tick % update_interval
+  for i=#global.VoidChests - offset, 1, -1 * update_interval do
+    global.VoidChests[i].clear_items_inside()
+    -- log("[VCP] "..game.tick.." clearing chest id "..global.VoidChests[i].unit_number)
   end
 end
 
--- tested with 504 chests at 100 tick update interval ~ 0.070ms
+-- tested with 480 chests over 100.000 ticks: 11805.281 ms
+-- function OnTick(event)
+  -- global.tickCount = global.tickCount or 1
+  -- global.chestIndex = global.chestIndex or 1
+
+  -- -- only work if index is within bounds
+  -- if global.chestIndex <= #global.VoidChests then
+    -- local stopIndex = global.chestIndex + global.VoidChestStride - 1
+    -- if stopIndex >= #global.VoidChests then
+      -- stopIndex = #global.VoidChests
+    -- end
+
+    -- -- log("[VCP] "..global.tickCount.." / "..game.tick.." clearing chest ids "..global.chestIndex.." to "..stopIndex)
+    -- for i=global.chestIndex, stopIndex do
+      -- global.VoidChests[i].get_inventory(defines.inventory.chest).clear()
+    -- end
+    -- global.chestIndex = stopIndex + 1
+  -- end
+
+  -- -- reset clock and chest index
+  -- if global.tickCount < update_interval then
+    -- global.tickCount = global.tickCount + 1
+  -- else
+    -- global.tickCount = 1
+    -- global.chestIndex = 1
+  -- end
+-- end
+
 -- function OnTick(event)
 	-- local tick = game.tick
 	-- for i=1, #global.VoidChests do
