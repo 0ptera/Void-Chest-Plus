47b4298ae7277e35220d75d04538ee8981d5ca8b
Optera <yousei.kyoko+github@gmail.com>
2019-03-22 07:20:31 +0100

    update git settings

---
+++ settings.lua
@@ -0,0 +1,10 @@
+data:extend({
+  {
+    type = "int-setting",
+    name = "vcp-update-interval",
+    order = "aa",
+    setting_type = "runtime-global",
+    default_value = 120,
+    minimum_value = 0
+  },
+})
