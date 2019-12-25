46288ff859ba557a0596be54f855173fd8e9f967
Optera <yousei.kyoko+github@gmail.com>
2019-12-25 11:41:05 +0100

    Version: 2.2.3 Date: 2019-12-25   Bugfixes:     - global scope variable caused desync #2

--- control.lua
+++ control.lua
@@ -1,25 +1,25 @@
 --[[ Copyright (c) 2017 Optera
- * Part of Void Chest Plus
- *
- * See LICENSE.md in the project directory for license information.
+* Part of Void Chest Plus
+*
+* See LICENSE.md in the project directory for license information.
 --]]
 
 ---- runtime Events ----
 function OnEntityCreated(event)
-	if event.created_entity.name == "void-chest" then
+  if event.created_entity.name == "void-chest" then
 
     event.created_entity.infinity_container_filters  = {}
     event.created_entity.remove_unfiltered_items = true
-	end
+  end
 end
 
 
-do---- Init ----
+do ---- Init ----
 local function init_chests()
   -- gather all void chests on every surface in case another mod added some
-	global.VoidChests = nil
-   for _, surface in pairs(game.surfaces) do
-    chests = surface.find_entities_filtered{ name = "void-chest" }
+  global.VoidChests = nil
+  for _, surface in pairs(game.surfaces) do
+    local chests = surface.find_entities_filtered{ name = "void-chest" }
     for _, chest in pairs(chests) do
       chest.infinity_container_filters = {}
       chest.remove_unfiltered_items = true
@@ -28,7 +28,7 @@ local function init_chests()
 end
 
 local function init_events()
-	script.on_event({defines.events.on_built_entity, defines.events.on_robot_built_entity}, OnEntityCreated)
+  script.on_event({defines.events.on_built_entity, defines.events.on_robot_built_entity}, OnEntityCreated)
 end
 
 script.on_load(function()
@@ -42,7 +42,7 @@ end)
 
 script.on_configuration_changed(function(data)
   init_chests()
-	init_events()
+  init_events()
 end)
 
 end
